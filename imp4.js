72e620a61dbb4ca71595dd289ac87d68f5072533
careercouncello <62845689+careercouncello@users.noreply.github.com>
2020-04-11 00:21:38 +0530

    Update imp4.js

--- imp4.js
+++ imp4.js
@@ -1,14 +1,15 @@
 function vishaln() {
   var g = b.toString();
   localStorage.setItem("FOODANDAGRICULTURE", g);
-  var v = 1;
-  var pq = v.toString();
-  localStorage.setItem("ale4", pq);
+
+  document.write('<body style="background:#c6ffdd"></body>');
   document.write(
-    "<h1>Congratulations You Have Successfully Completed the APPTITUDE TEST</h1>"
+    '<h1 style=" margin-left: 35%;@media screen(min-width:1200px){ margin-left: 20%;}">You have completed this section.</h1>'
   );
-  document.write("<h1>Click on the button for RESULT</h1>");
   document.write(
-    '<button id="mybutton"><a href="catagory.html">Catagory</a></button>'
+    '<h1 style=" margin-left: 35%;margin-right: 20%;">Click here to start another section.</h1>'
+  );
+  document.write(
+    '<a href="catagory.html" style="text-decoration: none; font-size:2rem; margin-left: 46%; font-weight: x-large; text-align:center; background-color: #013243; color: #fff;padding: 1%; border-radius: 5px;border-color: crimson;">category</a>'
   );
 }
